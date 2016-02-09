cf228033cf5d44b1e4dc820b03736deb451bdb1e
amuhs <muhsandrew@gmail.com>
2016-02-09 06:51:33 -0600

    home page delay improvement

--- js/home.js
+++ js/home.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
   $('.logo-1').delay(500).fadeIn(1000);
   $('.logo-1').fadeOut(1000);
 
-  $('.logo-2').delay(2500).fadeIn(1000);
+  $('.logo-2').delay(2600).fadeIn(1000);
   $('.logo-2').fadeOut(1000);
 
-  $('.name').delay(4500).fadeIn(2000);
+  $('.name').delay(4700).fadeIn(2000);
 });
