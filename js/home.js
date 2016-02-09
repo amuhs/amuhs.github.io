c4d289725056c91b3d709b6f809f399c73f6dd58
amuhs <muhsandrew@gmail.com>
2016-02-08 19:18:27 -0600

    Add new home page intro

---
+++ js/home.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.logo-1').delay(500).fadeIn(1000);
+  $('.logo-1').fadeOut(1000);
+
+  $('.logo-2').delay(2500).fadeIn(1000);
+  $('.logo-2').fadeOut(1000);
+
+  $('.name').delay(4500).fadeIn(2000);
+});
