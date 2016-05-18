ef35668839578a76f27cac55caf2c12f828295fb
amuhs <muhsandrew@gmail.com>
2016-05-18 17:10:09 -0500

    Add verse dropdown & force css styles

--- js/home.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('.logo-1').delay(500).fadeIn(1000);
-  $('.logo-1').fadeOut(1000);
-
-  $('.logo-2').delay(2600).fadeIn(1000);
-  $('.logo-2').fadeOut(1000);
-
-  $('.name').delay(4700).fadeIn(2000);
-});
