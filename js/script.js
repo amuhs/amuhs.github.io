a0a6231f39289303e474be477224357b909ff808
Andrew Muhs <muhsandrew@gmail.com>
2016-12-31 21:39:42 -0600

    Updates to verses - Added three new verse cards (10 - 12) - Merged app.js and script.js files - Added images for new project - Commented out incomplete project description - Added encoding declaration to all html files

--- js/script.js
+++ js/script.js
@@ -1,11 +1,18 @@
-$(function () {
-    var dropDown, week;
+$(function ()
+{
+
+    /*
+    *
+    * This section creates the dropdown menu when the page loads.
+    *
+    */
+    var dropDown, CURR_WEEK;
     dropDown = $("#versesDropDown");
-    week = 9;
+    CURR_WEEK = 12;
 
     for (var i = 1; i < 53; i++)
     {
-        if (i <= week)
+        if (i <= CURR_WEEK)
         {
             dropDown.append('<li value="' + i + '"><a href="#">Week ' + i + '</a></li>');
         }
@@ -15,11 +22,28 @@ $(function () {
         }
     }
 
+    /*
+    *
+    * This section changes the verse card based on what the user
+    * selects from the dropdown menu.
+    *
+    */
+    var ptOne, ptTwo, ptThree, weekNum
+    ptOne = "<a href=\"verse-cards/Week-";
+    ptTwo = ".png\"><img class=\"card\" src=\"verse-cards/Week-";
+    ptThree = ".png\" /></a>";
+
+    $('.dropdown-menu li').click(function()
+    {
+      weekNum = $(this).attr('value');
+      if (weekNum > CURR_WEEK)
+      {
+          $('.verse-card').html(ptOne + weekNum + ptTwo + 1 + ptThree);
+      }
+      else
+      {
+          $('.verse-card').html(ptOne + weekNum + ptTwo + weekNum + ptThree);
+      }
+    });
+
 });
-
-
-
-/*
-<li value="8"><a href="#">Week 8</a></li>
-<li class="disabled" value="' + i + '"><a href="#">Week ' + i + '</a></li>
-*/
